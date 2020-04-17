aff63b01473913a46c331295b4d199ebde26fdc5
lorenzo <lorenzo@lorenzo.info.com>
2020-04-17 15:13:49 +0200

    code

--- src/test/java/com/reedelk/json/component/ObjectToJSONTest.java
+++ src/test/java/com/reedelk/json/component/ObjectToJSONTest.java
@@ -5,6 +5,7 @@ import com.reedelk.runtime.api.flow.FlowContext;
 import com.reedelk.runtime.api.message.Message;
 import com.reedelk.runtime.api.message.MessageBuilder;
 import com.reedelk.runtime.api.message.content.DataRow;
+import com.reedelk.runtime.api.message.content.DefaultDataRow;
 import com.reedelk.runtime.api.message.content.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,8 +49,8 @@ class ObjectToJSONTest {
         // Given
         component.initialize();
 
-        DataRow<Serializable> row1 = TestDataRow.create(asList("id","name"), asList(4, "John Doe"));
-        DataRow<Serializable> row2 = TestDataRow.create(asList("id","name"), asList(3, "Mark Luis"));
+        DataRow<Serializable> row1 = DefaultDataRow.create(asList("id","name"), asList(4, "John Doe"));
+        DataRow<Serializable> row2 = DefaultDataRow.create(asList("id","name"), asList(3, "Mark Luis"));
 
         Message inMessage = MessageBuilder.get(TestComponent.class)
                 .withStream(Flux.just(row1, row2), DataRow.class)
@@ -78,7 +79,7 @@ class ObjectToJSONTest {
         // Given
         component.initialize();
 
-        DataRow<Serializable> row = TestDataRow.create(asList("id","name"), asList(4, "John Doe"));
+        DataRow<Serializable> row = DefaultDataRow.create(asList("id","name"), asList(4, "John Doe"));
 
         Message inMessage = MessageBuilder.get(TestComponent.class)
                 .withJavaObject(row)
